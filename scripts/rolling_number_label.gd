027da77698983010a010b004471691d9f102a307
dalefrob <dalef.rob@gmail.com>
2024-10-24 16:52:20 +0900

    gdscript doc for rollinglabel
    tried writing proper documentation for the rolling label class

--- scripts/rolling_number_label.gd
+++ scripts/rolling_number_label.gd
@@ -1,10 +1,13 @@
-## Used for numbers that visually count up or down to heighten impact
+## A label that will roll a numerical value up to the real value.
+## Used for visual impact of a value changing.
 extends Label
 class_name RollingNumberLabel
 
+## Add a word before the value such as HP:, or MP:
 @export var prefix : String = ""
 
-@export var value : int:
+## The real value
+@export var value : int = 0:
 	set(_value):
 		previous_value = value
 		value = _value
@@ -12,22 +15,24 @@ class_name RollingNumberLabel
 	get:
 		return value
 
-var previous_value : int # the value before the next one came in
-var rolling_value : int # the rolling value for animation
+## The value that gets set once a new value comes in
+var previous_value : int = 0 
 
+## Called once the label reaches the real value
 signal finished
 
 ## Sets the value straight away bypassing the roll
-func set_value_instantly(_value):
-	previous_value = _value
-	value = _value
+func set_value_instantly(new_value : int):
+	previous_value = new_value
+	value = new_value
 
+## Runs a tween that rolls the previous value to the new value
 func roll():
 	var tween = create_tween()
 	tween.tween_method(update_label, previous_value, value, 0.25)
 	tween.tween_callback(finished.emit)
 	tween.play()
 
+## Updates the text by joining the predix and number
 func update_label(number : int):
 	text = prefix + " " + str(number)
-	
